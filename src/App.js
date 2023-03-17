9548ef6cd0b0bea210d9f7aeab59bf01b980fb69
jcoegom@gmail.com <jcoegom@gmail.com>
2023-03-17 19:26:12 +0100

    routing done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
+import { Router, Link, Route } from "@reach/router";
 import "./App.css";
 
+import Home from "./pages/Home";
+import PodcastDetails from "./pages/PodcastDetails";
+import EpisodeDetails from "./pages/EpisodeDetails";
+
 function App() {
-  return <div className="App">Hi world</div>;
+  return (
+    <Router>
+      <Home path="/" />
+      <PodcastDetails path="/podcast/:podcastId" />
+      <EpisodeDetails path="/podcast/:podcastId/episode/:episodeId" />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/EpisodeDetails.js
@@ -0,0 +1,5 @@
+const EpisodeDetails = () => {
+  return <div>Episode Details</div>;
+};
+
+export default EpisodeDetails;
